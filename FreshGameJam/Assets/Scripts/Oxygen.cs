164db84cd8b9d466ddec21f7cb5631374757cec2
Connor William Roy Egan <connorwr.egan@gmail.com>
2020-07-21 10:28:51 +1000

    Create Oxygen Script
    The Oxygen Script Decreases Oxygen Like A Countdown.

---
+++ FreshGameJam/Assets/Scripts/Oxygen.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Oxygen : MonoBehaviour
+{
+public float maxOxygen = 10f;
+public float startOxygen = 10f;
+[HideInInspector]
+public float currentOxygen;
+
+public UnityEvent onOxygenOut;
+    
+
+
+
+    void Start() {
+        currentOxygen = startOxygen;
+    }
+    void Update() {
+        currentOxygen = Mathf.Clamp(currentOxygen, 0f, maxOxygen);
+
+        if (currentOxygen <= 0)
+            onOxygenOut.Invoke();
+        else
+            currentOxygen -= Time.deltaTime;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ FreshGameJam/Assets/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+
+
+
+
+
+
+
+
+}
